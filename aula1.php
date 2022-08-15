2dc9a71b5672cba48c7eed80be37f0320038ce25
walssimon <walssimon123@gmail.com>
2022-08-15 20:18:26 -0300

    Atualizacao 15 08 2022

--- aula1.php
+++ aula1.php
@@ -1,5 +1,20 @@
-<?php
-
-	echo "Olá Mundoo!!!";
-
+<?php 
+	
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php
+
+	echo "Olá mundo!!!";
+	?>
+
+</body>
+</html>
+
---
+++ aula3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<?php 
+	#Testando comentário de harsh marks
+	#Etec Irmã Agostina
+	
+ ?>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP - Testando comentário</title>
+</head>
+<body>
+
+	<?php 
+		/* este é um comentário 
+		de multiplas linhas. 
+		*/
+		acho "Segundo Exemplo em PHP"
+		$data = "20 de Agosto de 2020"
+
+		//Esse é um comentário de apenas uma linha 
+	?>
+
+	<p> Esse parágrafo está sendo interpretado como HTML</p>
+	Arquivo criado em <?php echo $data ?>
+
+
+</body>
+</html>
